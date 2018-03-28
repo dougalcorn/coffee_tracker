2487d9cc4a88b5b091d0b2e406b1a58b7cdcd7ae
Doug Alcorn <dougalcorn@gmail.com>
2018-03-28 08:06:42 -0400

    Fix warnings

--- test/support/fixtures.ex
+++ test/support/fixtures.ex
@@ -2,8 +2,8 @@ defmodule CoffeeTracker.Fixtures do
   alias CoffeeTracker.Coffee
 
   def insert_containers() do
-    {:ok, bag} = Coffee.create_container(%{name: "bag", unit: "g", weight: 0})
-    {:ok, cannister} = Coffee.create_container(%{name: "cannister", unit: "g", weight: 450})
-    {:ok, hopper} = Coffee.create_container(%{name: "hopper", unit: "g", weight: 375})
+    {:ok, _bag} = Coffee.create_container(%{name: "bag", unit: "g", weight: 0})
+    {:ok, _cannister} = Coffee.create_container(%{name: "cannister", unit: "g", weight: 450})
+    {:ok, _hopper} = Coffee.create_container(%{name: "hopper", unit: "g", weight: 375})
   end
 end
