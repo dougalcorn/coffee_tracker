f879e7b3379478348c7c7ec812aaff138ed12ba3
Doug Alcorn <dougalcorn@gmail.com>
2018-03-28 05:14:53 -0400

    Subtract container weight from DailyTotal

--- lib/coffee_tracker/coffee/daily_total.ex
+++ lib/coffee_tracker/coffee/daily_total.ex
@@ -13,7 +13,20 @@ defmodule CoffeeTracker.Coffee.DailyTotal do
 
   defp total_weight(measurements) do
     start = %{unit: "g", weight: 0}
-    Enum.reduce(measurements, start, &add_weight/2)
+    measurements
+    |> Enum.map(&subtract_container/1)
+    |> Enum.reduce(start, &add_weight/2)
+  end
+
+  defp subtract_container(%{container_id: container_id} = measurement) when is_nil(container_id), do: measurement
+
+  defp subtract_container(%{container_id: container_id} = measurement) do
+    container = Coffee.get_container!(container_id)
+    subtract_weight(measurement, container)
+  end
+
+  defp subtract_weight(%{unit: "g", weight: a}, %{unit: "g", weight: b}) do
+    %{unit: "g", weight: a - b}
   end
 
   defp add_weight(%{unit: "g", weight: a}, %{unit: "g", weight: b}) do
--- test/coffee_tracker/coffee/daily_total_test.exs
+++ test/coffee_tracker/coffee/daily_total_test.exs
@@ -30,5 +30,12 @@ defmodule CoffeeTracker.Coffee.DailyTotalTest do
       {:ok, _} = Coffee.create_measurement(%{date: date, unit: "g", weight: 400, type: "regular"})
       assert %DailyTotal{unit: "g", weight: ^total} = DailyTotal.get_daily_total!(date)
     end
+
+    test "subtracts out container weight" do
+      date = ~D[2018-03-28]
+      {:ok, hopper} = Coffee.create_container(%{name: "hopper", unit: "g", weight: 200})
+      {:ok, _} = Coffee.create_measurement(%{date: date, unit: "g", weight: 450, type: "regular", container_id: hopper.id})
+      assert %DailyTotal{weight: 250} = DailyTotal.get_daily_total!(date)
+    end
   end
 end
