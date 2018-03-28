78eecd0d24cfeb09280f89773cd910eafa1648bd
Doug Alcorn <dougalcorn@gmail.com>
2018-03-28 08:54:17 -0400

    Better display of measurements

--- lib/coffee_tracker_web/views/global_helpers.ex
+++ lib/coffee_tracker_web/views/global_helpers.ex
@@ -1,7 +1,11 @@
 defmodule CoffeeTrackerWeb.GlobalHelpers do
 
-  def display_measurement(%{weight: weight, unit: unit}, nil) do
-    "#{weight} #{unit}"
+  def display_measurement(%{weight: weight, unit: "g"}, nil) do
+    display_measurement(%{weight: weight / 454.0, unit: "lbm"}, nil)
+  end
+
+  def display_measurement(%{weight: weight, unit: "lbm"}, nil) do
+    "#{weight} lbm"
   end
 
   def display_measurement(measurement, %{name: name}) do
