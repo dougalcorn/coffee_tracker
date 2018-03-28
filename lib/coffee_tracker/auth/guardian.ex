7dd3633f1f562569890f81529e1235df6426a9f9
Doug Alcorn <dougalcorn@gmail.com>
2018-03-28 11:14:30 -0400

    Add basic authentication modules with guardian

---
+++ lib/coffee_tracker/auth/guardian.ex
@@ -0,0 +1,15 @@
+defmodule CoffeeTracker.Auth.Guardian do
+  use Guardian, otp_app: :coffee_tracker
+  alias CoffeeTracker.Auth
+
+  def subject_for_token(user, _claims) do
+    {:ok, to_string(user.id)}
+  end
+
+  def resource_from_claims(claims) do
+    user = claims["sub"]
+    |> Auth.get_user!
+    {:ok, user}
+    # If something goes wrong here return {:error, reason}
+  end
+end
