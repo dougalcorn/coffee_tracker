e06cb161bce2b5bf8e17d18f2a541b689d227020
Doug Alcorn <dougalcorn@gmail.com>
2018-05-03 09:19:30 -0400

    Handle no diffs

--- lib/coffee_tracker_web/controllers/daily_summary_controller.ex
+++ lib/coffee_tracker_web/controllers/daily_summary_controller.ex
@@ -6,9 +6,7 @@ defmodule CoffeeTrackerWeb.DailySummaryController do
   def index(conn, _params) do
     daily_totals = Coffee.list_daily_totals()
     daily_diffs = Coffee.list_daily_diffs(daily_totals)
-    first = hd(daily_diffs).date
-    last = Date.add(first, -14)
-    two_week_total_usage = Coffee.total_usage(daily_diffs, Date.range(first, last))
+    two_week_total_usage = Coffee.total_usage(daily_diffs, index_date_range(daily_diffs))
     render(conn, "index.html", daily_totals: daily_totals, daily_diffs: daily_diffs, two_week_total_usage: two_week_total_usage)
   end
 
@@ -17,5 +15,17 @@ defmodule CoffeeTrackerWeb.DailySummaryController do
     daily_total = Coffee.get_daily_total!(date)
     measurements = Coffee.list_daily_measurements(date)
     render(conn, "show.html", daily_total: daily_total, measurements: measurements)
+  end
+
+  defp index_date_range([]) do
+    first = Date.utc_today
+    last = Date.add(first, -14)
+    Date.range(first, last)
+  end
+
+  defp index_date_range(daily_diffs) do
+    first = hd(daily_diffs).date
+    last = Date.add(first, -14)
+    Date.range(first, last)
   end
 end
